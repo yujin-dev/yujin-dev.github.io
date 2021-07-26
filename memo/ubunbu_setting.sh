c48feec9bff94e8f7a2d5299c825054b8b26e75c
yujin-dev <qlcnal0211@gmail.com>
2021-07-26 12:20:57 +0900

    ubuntu_setting수정

--- memo/ubunbu_setting.sh
+++ memo/ubunbu_setting.sh
@@ -13,10 +13,16 @@ sudo apt-get install git
 pip3 install virtualenv
 # jupyter 
 pip3 install jupyter
+# visual studio code
+sudo apt-get install curl
+sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code
 # 원격
 sudo apt-get install remmina 
 # anydesk
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
-echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
-sudo apt update
-sudo apt install anydesk
+#wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+#echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+#sudo apt update
+#sudo apt install anydesk
